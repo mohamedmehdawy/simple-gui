71e0f8d9a9e83a9b5b39d575ec4084caa5736cb8
Mohamed Mehdawy <mohammedmehdawy@gmail.com>
2023-01-10 15:02:33 +0200

    work with border

--- master.py
+++ master.py
@@ -4,8 +4,20 @@ class SimpleGui:
     def __init__(self):
         # create main window widget
         self.tk = tkinter.Tk()
-        # set titlte for window
+        
+        # set size for window
+        self.tk.geometry("512x512")
+        
+        # set title for window
         self.tk.title('simple gui')
+        
+        # set label and put it to window
+        self.label1 = tkinter.Label(self.tk, text='hello world',borderwidth=2, relief='groove')
+        self.label2 = tkinter.Label(self.tk, text="hello mohamed")
+        self.label1.pack(side="left")
+        self.label2.pack(side="left")
+        
+        
         # enter the program to main loop
         self.tk.mainloop()
         
