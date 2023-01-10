1076a594fc8c594631b6864501a4b94c15716907
Mohamed Mehdawy <mohammedmehdawy@gmail.com>
2023-01-10 21:31:16 +0200

    work with frames

--- master.py
+++ master.py
@@ -11,12 +11,25 @@ class SimpleGui:
         # set title for window
         self.tk.title('simple gui')
         
-        # set label and put it to window
-        self.label1 = tkinter.Label(self.tk, text='hello world',borderwidth=2, relief='groove')
-        self.label2 = tkinter.Label(self.tk, text="hello mohamed")
-        self.label1.pack(side="left")
-        self.label2.pack(side="left")
+        # create frames
+        self.top_frame = tkinter.Frame(self.tk)
+        self.bottom_frame = tkinter.Frame(self.tk)
         
+        # set labels and put it to top frame
+        self.label1 = tkinter.Label(self.top_frame, text='hello world',borderwidth=2, relief='solid')
+        self.label2 = tkinter.Label(self.top_frame, text='hello mohamed', borderwidth=2, relief='solid')
+        self.label1.pack(ipadx=10, ipady=10, padx=10, pady=10)
+        self.label2.pack(ipadx=10, ipady=10, padx=10, pady=10)
+        
+        # set labels and put it to bottom fram
+        self.label2 = tkinter.Label(self.bottom_frame, text='my age is 32',borderwidth=2, relief='solid')
+        self.label3 = tkinter.Label(self.bottom_frame, text='bla bla bla', borderwidth=2, relief='solid')
+        self.label2.pack(ipadx=10, ipady=10, padx=10, pady=10, side='left')
+        self.label3.pack(ipadx=10, ipady=10, padx=10, pady=10, side='left')
+        
+        # register frames to main window
+        self.top_frame.pack()
+        self.bottom_frame.pack()
         
         # enter the program to main loop
         self.tk.mainloop()
